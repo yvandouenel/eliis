9798c5b8d827ecbaf9a31de51b550b7e6a680081
Yvan <y.douenel@coopernet.fr>
2015-08-24 20:10:59 +0200

    eliis.info and page.tpl.php modified.

--- sites/all/themes/eliis/templates/page.tpl.php
+++ sites/all/themes/eliis/templates/page.tpl.php
@@ -74,101 +74,122 @@
  */
 ?>
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?> navbar-fixed-top">
-  <div class="container">
-    <div class="navbar-header">
-      <?php if ($logo): ?>
-      <h1 id="h1-logo"><a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-        <img src="<?php print $logo; ?>" alt="Eliis - Software Solutions in Seismic Interpretation - <?php print t('Home'); ?>" />
-      </a></h1>
-      <?php endif; ?>
+    <div class="container">
+        <div class="navbar-header">
+            <?php if ($logo): ?>
+                <h1 id="h1-logo"><a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>"
+                                    title="<?php print t('Home'); ?>">
+                        <img src="<?php print $logo; ?>"
+                             alt="Eliis - Software Solutions in Seismic Interpretation - <?php print t('Home'); ?>"/>
+                    </a></h1>
+            <?php endif; ?>
 
-      <?php if (!empty($site_name)): ?>
-      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
+            <?php if (!empty($site_name)): ?>
+                <a class="name navbar-brand" href="<?php print $front_page; ?>"
+                   title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+            <?php endif; ?>
 
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
+            <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+
+        <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+            <div class="navbar-collapse collapse">
+                <nav role="navigation">
+                    <?php if (!empty($primary_nav)): ?>
+                        <?php print render($primary_nav); ?>
+                    <?php endif; ?>
+                    <?php if (!empty($secondary_nav)): ?>
+                        <?php print render($secondary_nav); ?>
+                    <?php endif; ?>
+                    <?php if (!empty($page['navigation'])): ?>
+                        <?php print render($page['navigation']); ?>
+                    <?php endif; ?>
+                </nav>
+            </div>
+        <?php endif; ?>
     </div>
-
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
-      </div>
-    <?php endif; ?>
-  </div>
 </header>
 
 <div class="main-container container">
 
-  <header role="banner" id="page-header">
-    <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
+    <header role="banner" id="page-header">
+        <?php if (!empty($site_slogan)): ?>
+            <p class="lead"><?php print $site_slogan; ?></p>
+        <?php endif; ?>
 
-    <?php print render($page['header']); ?>
-  </header> <!-- /#page-header -->
+        <?php print render($page['header']); ?>
+    </header>
+    <!-- /#page-header -->
 
-  <div class="row">
+    <div class="row">
 
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
-    <?php endif; ?>
+        <?php if (!empty($page['sidebar_first'])): ?>
+            <aside class="col-sm-3" role="complementary">
+                <?php print render($page['sidebar_first']); ?>
+            </aside>  <!-- /#sidebar-first -->
+        <?php endif; ?>
 
-    <section<?php print $content_column_class; ?>>
-      <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-      <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h2 class="page-header"><?php print $title; ?></h2>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-    </section>
+        <section<?php print $content_column_class; ?>>
+            <?php if (!empty($page['highlighted'])): ?>
+                <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+            <?php endif; ?>
+            <?php if (!empty($breadcrumb)): print $breadcrumb; endif; ?>
+            <a id="main-content"></a>
+            <?php print render($title_prefix); ?>
+            <?php if (!empty($title)): ?>
+                <h2 class="page-header"><?php print $title; ?></h2>
+            <?php endif; ?>
+            <?php print render($title_suffix); ?>
+            <?php print $messages; ?>
+            <?php if (!empty($tabs)): ?>
+                <?php print render($tabs); ?>
+            <?php endif; ?>
+            <?php if (!empty($page['help'])): ?>
+                <?php print render($page['help']); ?>
+            <?php endif; ?>
+            <?php if (!empty($action_links)): ?>
+                <ul class="action-links"><?php print render($action_links); ?></ul>
+            <?php endif; ?>
+            <?php print render($page['content']); ?>
+        </section>
 
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>  <!-- /#sidebar-second -->
-    <?php endif; ?>
+        <?php if (!empty($page['sidebar_second'])): ?>
+            <aside class="col-sm-3" role="complementary">
+                <?php print render($page['sidebar_second']); ?>
+            </aside>  <!-- /#sidebar-second -->
+        <?php endif; ?>
 
-  </div>
+    </div>
+    <div class="region-footer-cols">
+        <div class="col-md-4">
+            <?php print render($page['footer-col1']); ?>
+        </div>
+        <div class="col-md-4">
+            <?php print render($page['footer-col2']); ?>
+        </div>
+        <div class="col-md-4">
+            <?php print render($page['footer-col3']); ?>
+        </div>
+    </div>
 </div>
 <footer class="footer container">
+
     <div class="social-courtesy">
         <?php print render($page['footer']); ?>
     </div>
     <div id="footer-adress">
-        <div id="adress-eliis"><img src="<?php print($directory) ?>/images/logo-eliis-small.gif" alt="logo Eliis" style="vertical-align:middle;"> Parc Mermoz, Immeuble l'Onyx - 187, rue Hélène Boucher - 34 170 Castelnau le Lez - France | <a href="mailto:contact@eliis.fr">contact@eliis.fr</a> | Tel : +33(0) 4 67 41 31 16 | Fax : +33 (0)4 30 96 60 04 </div>
-        <div id="coopernet"><a target="_blank" href="http://www.coopernet.fr">Coopernet : web site creation with drupal in Montpellier</a></div>
+        <div id="adress-eliis"><img src="<?php print($directory) ?>/images/logo-eliis-small.gif" alt="logo Eliis"
+                                    style="vertical-align:middle;"> Parc Mermoz, Immeuble l'Onyx - 187, rue Hélène
+            Boucher - 34 170 Castelnau le Lez - France | <a href="mailto:contact@eliis.fr">contact@eliis.fr</a> | Tel :
+            +33(0) 4 67 41 31 16 | Fax : +33 (0)4 30 96 60 04
+        </div>
+        <div id="coopernet"><a target="_blank" href="http://www.coopernet.fr">Coopernet : web site creation with drupal
+                in Montpellier</a></div>
     </div>
 </footer>
