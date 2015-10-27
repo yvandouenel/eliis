30786cfbe909c6bef85553503536742a22ccd73a
Yvan Douenel <y.douenel@coopernet.fr>
2015-10-27 11:14:03 +0100

    template modified.

--- sites/all/themes/eliis/template.php
+++ sites/all/themes/eliis/template.php
@@ -11,3 +11,11 @@ function eliis_preprocess_image(&$variables) {
         unset($variables[$key]);
     }
 }
+function eliis_preprocess_html(&$vars, $hook) {
+    if ($vars['user']) {
+        foreach($vars['user']->roles as $key => $role){
+            $role_class = 'role-' . drupal_clean_css_identifier($role);
+            $vars['classes_array'][] = $role_class;
+        }
+    }
+}
