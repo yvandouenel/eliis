58b667245fb9d23f70a4eab1dee37a287e8f939e
Yvan Douenel <y.douenel@coopernet.fr>
2016-01-09 17:56:22 +0100

    template.php modified

--- sites/all/themes/eliis/template.php
+++ sites/all/themes/eliis/template.php
@@ -19,3 +19,32 @@ function eliis_preprocess_html(&$vars, $hook) {
         }
     }
 }
+
+/*function eliis_preprocess_field(&$vars) {
+
+    if($vars['element']['#field_name'] == "field_sofware"){
+        dpm("hello field_sofware");
+        $vars['classes_array'][] = 'bobobo';
+        $classes = &$vars['classes_array'];
+        dpm($classes);
+        $vars['classes_array'][] = 'bobobo';
+
+    }
+}*/
+function eliis_preprocess_field(&$vars) {
+
+    if($vars['element']['#field_name'] == "field_files_to_download"
+    ){
+        $fcs = array();
+        foreach (element_children($vars['element']) as $key) {
+            $fcs[] = array_pop($vars['element'][$key]['entity']['field_collection_item']);
+        }
+        // now you have all your fcs pulled out of that nasty array - cf http://dropbucket.org/node/764
+        foreach ($fcs as $fc) {
+            if (isset($fc['field_color_link_download']['#items'][0]['value']) &&
+              $fc['field_color_link_download']['#items'][0]['value'] == 'red'){
+                $vars['classes_array'][] = 'red-link-download';
+            }else $vars['classes_array'][] = 'blue-link-download';
+        }
+    }
+}
