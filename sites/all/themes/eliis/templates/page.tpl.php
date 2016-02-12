00e904ae07fcee76de3c9471c3510dbbc6030147
Yvan Douenel <y.douenel@coopernet.fr>
2016-02-12 15:20:26 +0100

    page.tpl.php up.

--- sites/all/themes/eliis/templates/page.tpl.php
+++ sites/all/themes/eliis/templates/page.tpl.php
@@ -188,7 +188,7 @@
         <div id="adress-eliis"><img src="/<?php print($directory) ?>/images/logo-eliis-small.gif" alt="logo Eliis"
                                     style="vertical-align:middle;"> Parc Mermoz, Immeuble l'Onyx - 187, rue Hélène
             Boucher - 34 170 Castelnau le Lez - France | <a href="mailto:contact@eliis.fr">contact@eliis.fr</a> | Tel :
-            +33(0) 4 67 41 31 16 | Fax : +33 (0)4 30 96 60 04
+            +33(0) 4 67 41 31 16
         </div>
         <div id="coopernet"><a target="_blank" href="http://www.coopernet.fr">Coopernet : drupal website creation in Montpellier</a></div>
     </div>
