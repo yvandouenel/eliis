7f5b286bb5d45fca8007dbc560ec3e43da88f35c
Yvan Douenel <y.douenel@coopernet.fr>
2018-02-02 10:55:28 +0100

    feature_eliis_support up to manage videos as commanded by Eliis.

--- sites/all/modules/features/feature_eliis_support/js/diaporama_video.js
+++ sites/all/modules/features/feature_eliis_support/js/diaporama_video.js
@@ -1,31 +1,39 @@
 /**
  * Created by yvan on 07/12/15.
  */
-jQuery(function ($) {
-  var numberid;
-  $("#big-video").hide();
+Drupal.behaviors.feature_eliis_support = {
+  attach: function (context, settings) {
+    console.log("suis dans diaporama_video.js");
+    var numberid;
+    jQuery("#big-video").hide();
 
-  $("#big-video").prepend('<i id="close-button"></i>');
-  $("#close-button").on("click",function(){
-    if ($("#big-video iframe").length) {
-      numberid = $("#big-video iframe").attr("class");
-      $("#big-video iframe").appendTo("#" + numberid).addClass("video-viewed");
+    if (!jQuery("#close-button").length) {
+      jQuery("#big-video").prepend('<i id="close-button"></i>');
+      jQuery("#close-button").on("click",function(){
+        if (jQuery("#big-video iframe").length) {
+          numberid = jQuery("#big-video iframe").attr("class");
+          jQuery("#big-video iframe").appendTo("#" + numberid);
+          jQuery("#" + numberid).addClass("video-viewed");
+        }
+        jQuery("#big-video").hide();
+      });
+      jQuery(".group-video-wrapper .col-md-2 .field-content > div").each(function (index) {
+          var cl = jQuery(this).attr("id");
+          jQuery("iframe", jQuery(this)).attr("class", cl);
+        }
+      );
+      jQuery(".group-video-wrapper .views-row").on("click",function(){
+        jQuery("#big-video").show();
+        if (jQuery("#big-video iframe").length) {
+          numberid = jQuery("#big-video iframe").attr("class");
+          jQuery("#big-video iframe").appendTo("#" + numberid);
+          jQuery("#" + numberid).addClass("video-viewed");
+        }
+        jQuery("iframe", jQuery(this)).appendTo("#big-video");
+      });
     }
-    $("#big-video").hide();
-  });
 
-  $(".group-video-wrapper .col-md-2 .field-content > div").each(function (index) {
-      var cl = $(this).attr("id");
-      $("iframe", $(this)).attr("class", cl);
-    }
-  );
-  $(".group-video-wrapper .views-row").on("click",function(){
-    $("#big-video").show();
-    if ($("#big-video iframe").length) {
-      numberid = $("#big-video iframe").attr("class");
-      $("#big-video iframe").appendTo("#" + numberid).addClass("video-viewed");;
-    }
-    $("iframe", $(this)).appendTo("#big-video");
-  });
 
-});
+  }
+};
+
